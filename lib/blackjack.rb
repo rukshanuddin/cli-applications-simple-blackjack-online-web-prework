0f7660e1a64ae6cf192422058ef155b0b9f20ae0
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-10-14 16:07:16 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -55,9 +55,8 @@ def runner
   welcome
   total = initial_round
   until total > 21
-    hit?(total)
-    end
-
-display_card_total(total)
+    total = hit?(total)
+    display_card_total(total)
+  end
 end_game(total)
 end
